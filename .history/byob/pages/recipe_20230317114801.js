ac871e5dc5ce48b292fd33b3635806b636f9b100
marisalen <mlenarduzzi1@my.bcit.ca>
2023-03-17 13:12:28 -0700

    passedinfo to recipepg/styled

---
+++ .history/byob/pages/recipe_20230317114801.js
@@ -0,0 +1,47 @@
+import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
+
+const Recipe = () => {
+  const router = useRouter();
+  const [cocktail, setCocktail] = useState(null);
+
+  useEffect(() => {
+    const fetchCocktail = async () => {
+      const { id } = router.query;
+      const response = await fetch(
+        `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`
+      );
+      const data = await response.json();
+      setCocktail(data.drinks[0]);
+    };
+    fetchCocktail();
+  }, []);
+
+  return (
+    <div>
+      {cocktail ? (
+        <div>
+          <h1>{cocktail.strDrink}</h1>
+          <img src={cocktail.strDrinkThumb} alt={cocktail.strDrink} />
+          <h2>Ingredients:</h2>
+          <ul>
+            {Object.keys(cocktail)
+              .filter((key) => key.startsWith("strIngredient"))
+              .map((key) => {
+                if (cocktail[key]) {
+                  return <li key={key}>{cocktail[key]}</li>;
+                }
+                return null;
+              })}
+          </ul>
+          <h2>Instructions:</h2>
+          <p>{cocktail.strInstructions}</p>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+};
+
+export default Recipe;
---
+++ .history/byob/pages/recipe_20230317115428.js
@@ -0,0 +1,64 @@
+import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
+import styled from 'styled-components';
+
+
+const Logo = styled.img`
+margin-top: -4rem;
+width: 18rem;
+display: flex;
+`
+const Banner = styled.img`
+display: flex;
+align-items:baseline;
+margin-top: -2rem;
+`
+const ButtonCont = styled.div`
+display: flex;
+padding: 10px;
+`
+
+const Recipe = () => {
+  const router = useRouter();
+  const [cocktail, setCocktail] = useState(null);
+
+  useEffect(() => {
+    const fetchCocktail = async () => {
+      const { id } = router.query;
+      const response = await fetch(
+        `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`
+      );
+      const data = await response.json();
+      setCocktail(data.drinks[0]);
+    };
+    fetchCocktail();
+  }, []);
+
+  return (
+    <div>
+      {cocktail ? (
+        <div>
+          <h1>{cocktail.strDrink}</h1>
+          <img src={cocktail.strDrinkThumb} alt={cocktail.strDrink} />
+          <h2>Ingredients:</h2>
+          <ul>
+            {Object.keys(cocktail)
+              .filter((key) => key.startsWith("strIngredient"))
+              .map((key) => {
+                if (cocktail[key]) {
+                  return <li key={key}>{cocktail[key]}</li>;
+                }
+                return null;
+              })}
+          </ul>
+          <h2>Instructions:</h2>
+          <p>{cocktail.strInstructions}</p>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+};
+
+export default Recipe;
