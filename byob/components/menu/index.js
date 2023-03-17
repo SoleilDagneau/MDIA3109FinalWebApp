5bba97d3e9d8398263300e29af490d0b166b2b2a
soleildagneau <sdagneau@my.bcit.ca>
2023-03-17 16:28:10 -0700

    menu

---
+++ byob/components/menu/index.js
@@ -0,0 +1,36 @@
+import Link from 'next/link';
+import styles from '@/styles/Home.module.css'
+
+import styled from "styled-components";
+
+const MenuCont = styled.div`
+position: absolute;
+top: 65px;
+left: 0;
+background-color: #D8334F;
+padding: 10px;
+display: flex;
+flex-direction: column;
+align-items: flex-end;
+margin: 5px 0;
+font-family: 'Quicksand', sans-serif;
+color: white;
+`
+
+const Menu = () => {
+  return (
+    <MenuCont>
+      <Link href="/">
+        Home
+      </Link>
+      <Link href="/results">
+        Search
+      </Link>
+      <Link href="/map">
+       Map
+      </Link>
+    </MenuCont>
+  );
+};
+
+export default Menu;
